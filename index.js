8976d85114b14b5bfff9dd1e44225e772564a7c5
yashgupta31 <yashsntoshgupta2019@gmail.com>
2024-07-14 00:54:14 +0530

    cors added

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express= require("express");
 const connection= require("./config/db")
 const userRouter= require("./routes/user.route")
 const noteRouter= require("./routes/note.route")
+const cors = require("cors"); // Import cors
 const dotenv= require("dotenv")
 dotenv.config();
 
-const PORT= process.env.PORT
+const PORT= process.env.PORT || 4000
 const app= express();
+
+app.use(cors()); // Use cors middleware to allow all origins
 app.use(express.json());
 app.use('/user',userRouter)
 app.use('/note', noteRouter)
