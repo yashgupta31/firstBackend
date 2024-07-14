c67040287c0951be8ea4c7f9a660266a9827a95a
yashgupta31 <yashsntoshgupta2019@gmail.com>
2024-07-14 20:37:28 +0530

    done

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ const app= express();
 
 app.use(cors());
 app.options('*', cors());
-app.use(function(req, res, next) {
+app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 app.use(express.json());
